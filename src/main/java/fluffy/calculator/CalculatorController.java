4bef1eac446d2db4f5323c4c78058a5367fad364
Силинский Игорь Александрович <Silinskij.IA@esphere.local>
2018-06-26 18:24:20 +0300

    add package fluffy.calculator to CalculatorController

--- src/main/java/fluffy/calculator/CalculatorController.java
+++ src/main/java/fluffy/calculator/CalculatorController.java
@@ -1,3 +1,4 @@
+package fluffy.calculator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
